828b7c39e8230ded81731e8f0387d73d6d52322a
kcissel <golovina.nastya02@gmail.com>
2024-06-05 19:40:49 +0500

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -2,8 +2,8 @@ name = 'Anastasia'
 print('Name:', name)
 age = 20
 print('Age:', age)
-new_age = 22
-print('New_age:', new_age)
+age = age+2
+print('New_age:', age)
 is_student = True
 print('Is_student:', is_student)
 
